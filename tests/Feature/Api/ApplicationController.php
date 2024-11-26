4d2ced7e3f0e66d784377411e119f13b448dab2f
issamchlf <issamchellaf734@gmail.com>
2024-11-26 10:32:24 +0100

    feat:add tests

--- tests/Feature/Api/ApplicationController.php
+++ tests/Feature/Api/ApplicationController.php
@@ -24,6 +24,79 @@ class ApplicationController extends TestCase
     }
     public function test_CheckIfCanDeleteAnEntryOnApplicationWithApi(){
 
-        $applications = Application::factrory(2)->
+        $applications = Application::factrory(2)->create();
+
+        $this->delete(route('apidestroy', $applications->first()->id));
+
+        $this->assertDatabaseCount('application', 1);
+
+        $response  = $this->get(route('apihome'));
+
+        $response->assertStatus(200)->assertJsonCount(1);
+
+    }
+
+    public function test_CheckICanCreateAnEntryOnApplicationWithApi()
+    {
+        $response = $this->post(route('apistore'),[
+            'job title' => 'Test JobTitle',
+            'status' => 'Test Status',
+            'category' => 'Test Categpry'
+        ]);
+
+        $response = $this->get(route('apihome'));
+        $response->assertStatus(200)->assertJsonCount(1);
+
+    }
+    public function test_checkIfCanUpdateAnEntryOnApplicationWithApi()
+    {
+        $this->post(route('apistore'),[
+            'job title' => 'Test JobTitle',
+            'status' => 'Test Status',
+            'category' => 'Test Category'  
+        ]);
+
+        $data = [
+            'job title' => 'Test JobTitle'
+        ];
+
+        $response = $this->get(route('apihome'));
+        $response->assertStatus(200)
+            ->assertJsonCount(1)
+            ->assertJsonFragment($data);
+        
+        $ApplicationId = $response->json()[0]['id'];
+
+        $this->put(route('apiupdate', $ApplicationId),[
+            'job title' => 'Test JobTitle Updated',
+            'status' => 'Test Status Updated',
+            'category' => 'Test Category Updated'
+        ]);
+
+        $newData = [
+            'job title' => 'Test JobTitle Updated',
+        ];
+
+        $response = $this->get(route('apihome'));
+        $response->assertStatus(200)
+            ->assertJsonCount(1)
+            ->assertJsonFragment($newData);
+        
+    }
+
+    public function test_CheckIfCanFindAnEntryByIdOnApplicationWithApi()
+    {
+        $this->post(route('apistore'),[
+            'job title' => 'Test JobTitle',
+            'status' => 'Test Status',
+            'category' => 'Test Category'
+        ]);
+        $data = [
+            'job title' => 'Test JobTitle',   
+        ];
+        $response = $this->get(route('apishow', 1));
+        $response->assertStatus(200)
+            ->assertJsonCount(5)
+            ->assertJsonFragment($data);
     }
 }
