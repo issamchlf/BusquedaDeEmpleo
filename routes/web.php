505330270712c8c9c6f0daae228e448347501bf0
issamchlf <issamchellaf734@gmail.com>
2024-11-28 11:42:05 +0100

    refactor:delete wrong files and add test of offer controller

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApplicationController;
 
 Route::get('/', [ApplicationController::class, 'index'])->name('home');
-Route::get('/applications{id}',[ApplicationController::class, 'show'])->name('/application.show');
+Route::get('/offer{id}',[ApplicationController::class, 'show'])->name('/offer.show');
--- tests/Feature/offertest.php
+++ tests/Feature/offertest.php
@@ -2,19 +2,23 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\offer;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class offertest extends TestCase
+class offerTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_ListOfEntryCanBeRead()
     {
-        $response = $this->get('/');
+        $this->withoutExceptionHandling();
+        offer::all();
+        $response = $this->get('/offer{id}');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+            ->assertViewIs('/offer.show');
     }
 }
