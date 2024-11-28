d7a675e085dc988db8dbe40da6a01b16e685ab61
issamchlf <issamchellaf734@gmail.com>
2024-11-28 11:30:05 +0100

    feat:add test apiOffer

--- tests/Feature/Api/offerControllerTest.php
+++ tests/Feature/Api/offerControllerTest.php
@@ -12,5 +12,65 @@ class offerControllerTest extends TestCase
     /**
      * A basic feature test example.
      */
-    
+    public function test_CheckICanCreateAnEntryOnOfferWithApi(){
+        offer::factory(2)->create();
+
+        $response = $this->get(route('apishow'));
+        $response->assertStatus(200)->assertJsonCount(2);
+    }
+    public function test_CheckIfCanDeleteAnEntryOnOfferWithApi(){
+        $offer = offer::factory(2)->create();
+
+        $this->delete(route('apidestroy', $offer->first()->id));
+
+        $this->assertDatabaseCount('offer', 1);
+
+        $response = $this->get(route('apishow'));
+
+        $response->assertStatus(200)->assertJsonCount(1);
+
+    }
+    public function test_CheckIfCanCreateAnEntryOnOfferWithApi(){
+        $response = $this->post(route('apistore'),[
+            'company_name' => 'Test company_name',
+            'location' => 'Test location',
+            'comment' => 'Test comment',
+            'salary' => 'Test salary'
+        ]);
+        $response = $this->get(route('apishow'));
+        $response->assertStatus(200)->assertJsonCount(1);
+    }
+
+    public function test_checkIfCanUpdateAnEntryOnOfferWithApi(){
+        $this->post(route('apistore'),[
+            'company_name' => 'Test company_name',
+            'location' => 'Test location',
+            'comment' => 'Test comment',
+            'salary' => 'Test salary'
+        ]);
+        $data = [
+            'company_name' => 'Test company_name'
+        ];
+        $response = $this->get(route('apishow'));
+        $response->assertStatus(200)
+            ->asserJsonCount(1)
+            ->assertJsonFragment($data);
+        
+        $offerId = $response->json()[0]['id'];
+
+        $this->put(route('apiupdate', $offerId),[
+            'company_name' => 'Test company_name Updated',
+            'location' => 'Test location Updated',
+            'comment' => 'Test comment Updated',
+            'salary' => 'Test salary Updated'
+        ]);
+
+        $newData = [
+            'company_name' => 'Test company_name Updated',
+        ];
+        $response = $this->get(route('apishow'));
+        $response->assertStatus(200)
+            ->assertJsonCount(1)
+            ->assertJsonFragment($newData);
+    }
 }
