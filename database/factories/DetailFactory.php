89ea7e06fa7f8efc00121c8e431fd2ce55501a53
issamchlf <issamchellaf734@gmail.com>
2024-11-29 14:19:00 +0100

    refactor:delete unessecary factories

--- database/factories/DetailFactory.php
+++ database/factories/DetailFactory.php
@@ -17,7 +17,14 @@ class DetailFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            //'work_id'
+            //'company_name' => $this->faker->varchar(),
+            //'location' => $this->faker->varchar(),
+            //'comment' => $this->faker->realText(10), 
+            //'salary' => $this->faker->numberBetween(30000, 120000),
+            //'URL' => $this->faker->;
+         
+
         ];
     }
 }
