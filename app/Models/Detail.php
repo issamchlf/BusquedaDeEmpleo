2502f9143130f2b394e234c504068c4043dcb400
issamchlf <issamchellaf734@gmail.com>
2024-11-29 14:05:03 +0100

    refactor:delete useless files

--- app/Models/Detail.php
+++ app/Models/Detail.php
@@ -2,14 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Detail extends Model
 {
+    use HasFactory;
+    protected $fillable = [
     "work_id",
     "company name",
     "location",
     "comment",
     "salary",
     "URL"
+    ];
 }
