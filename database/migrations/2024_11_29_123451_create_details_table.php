38f5cd580a1f95b92213bbc42c77e6aa80b31c66
issamchlf <issamchellaf734@gmail.com>
2024-11-29 13:36:22 +0100

    feat:add new table for change the correct subjects

--- database/migrations/2024_11_29_123451_create_details_table.php
+++ database/migrations/2024_11_29_123451_create_details_table.php
@@ -11,13 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('works', function (Blueprint $table) {
+        Schema::create('details', function (Blueprint $table) {
             $table->id();
-            $table->work_id();
-            $table->string("company_name");
-            $table->string("location");
-            $table->text("comment");
-            $table->bigInteger("salary");
             $table->timestamps();
         });
     }
@@ -27,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('works');
+        Schema::dropIfExists('details');
     }
 };
